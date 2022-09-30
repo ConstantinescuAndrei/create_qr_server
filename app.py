a6970ed9bf31dd395a6030d56e247386475f421d
andrei <constantinescu321@gmail.com>
2022-09-30 05:22:57 +0300

    Return base64 image

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import qrcode
 from flask import Flask, send_file, request
+from flask_cors import CORS
+import base64
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/create-qr', methods=['POST'])
 def create_qr():
@@ -12,8 +15,9 @@ def create_qr():
         menu_link = payload['menu_link']
         img = qrcode.make("http://localhost:3000/" + menu_link)
         img.save("qrcode.png")
+        encoded = base64.b64encode(open("qrcode.png", "rb").read())
 
-        return send_file('qrcode.png')
+        return encoded
 
     return 'bad rerquest!', 400
 
